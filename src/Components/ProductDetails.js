fe165cea2735336d77c996189f8902a56eaba63d
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-08 21:29:47 -0400

    created products details, imported hooks

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,5 +1,18 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
 export default function ProductDetails() {
-  return (<div className="ProductDetails">
-    ProductDetails
-  </div>)
+  const API = process.env.REACT_APP_API_URL;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/products/${id}`)
+      .then((res) => setProduct(res.data), [id, navigate, API]);
+  });
+
+  return <div className="ProductDetails">ProductDetails</div>;
 }
