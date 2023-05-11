0233e8d6bb89901529a860c266fd16397168a966
BertCasale <bthebest55@gmail.com>
2023-05-11 09:37:17 -0400

    product css

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,12 +1,23 @@
+import "./Product.css";
+import {Link} from "react-router-dom";
+
 export default function Product({ product }) {
-  return (
-    <div className="Product">
-      <img src={product.image_url} />
-      <h3>{product.name}</h3>
-      <h4>{product.brand}</h4>
-      <strong>
-        <h5>${product.price}</h5>
-      </strong>
-    </div>
-  );
+  return (<div className="Product">
+    <Link to={`/products/${product.id}`}>
+      <div className="product-card">
+        <div className="image">
+          <img src={product.image_url} />
+        </div>
+
+        <div className="info">
+          <h3>{product.name}</h3>
+        <h4>{product.brand}</h4>
+        <strong>
+          <h5>${product.price}</h5>
+        </strong>
+        </div>
+        
+      </div>
+    </Link>
+  </div>);
 }
